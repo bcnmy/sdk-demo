75d3e1da9e130355e84a85f91011400ca508ce7f
Joe Pegler <joepegler123@gmail.com>
2024-06-28 17:55:31 +0100

    chore: lint

--- src/components/Modules/CreateDanSession.tsx
+++ src/components/Modules/CreateDanSession.tsx
@@ -6,22 +6,22 @@ import {
   SessionLocalStorage,
   type Transaction,
   createDANSessionKeyManagerModule,
-  createSessionKeyEOA,
-  getChain,
   createERC20SessionDatum,
-} from "@biconomy/account";
-import { bigIntReplacer, useSmartAccount } from "@biconomy/use-aa";
-import { makeStyles } from "@mui/styles";
-import * as ed from "@noble/ed25519";
-import { ethers } from "ethers";
-import type React from "react";
-import { useEffect, useState } from "react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { type Hex, keccak256, parseUnits, encodeAbiParameters } from "viem";
-import { useAccount, useClient } from "wagmi";
-import Button from "../Button";
-import UseDanSession from "./UseDanSession";
+  createSessionKeyEOA,
+  getChain
+} from "@biconomy/account"
+import { bigIntReplacer, useSmartAccount } from "@biconomy/use-aa"
+import { makeStyles } from "@mui/styles"
+import * as ed from "@noble/ed25519"
+import { ethers } from "ethers"
+import type React from "react"
+import { useEffect, useState } from "react"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import { type Hex, encodeAbiParameters, keccak256, parseUnits } from "viem"
+import { useAccount, useClient } from "wagmi"
+import Button from "../Button"
+import UseDanSession from "./UseDanSession"
 
 import {
   EOAAuth,
@@ -29,49 +29,49 @@ import {
   type KeygenResponse,
   NetworkSigner,
   type TypedData,
-  WalletProviderServiceClient,
-} from "@silencelaboratories/walletprovider-sdk";
-import { configInfo } from "../../utils";
+  WalletProviderServiceClient
+} from "@silencelaboratories/walletprovider-sdk"
+import { configInfo } from "../../utils"
 
-let ephSK: Uint8Array | null = null;
-let ephPK: Uint8Array | null = null;
+let ephSK: Uint8Array | null = null
+let ephPK: Uint8Array | null = null
 
 interface EIP6963ProviderInfo {
-  uuid: string;
-  name: string;
-  icon: string;
-  rdns: string;
+  uuid: string
+  name: string
+  icon: string
+  rdns: string
 }
 
 interface RequestArguments {
-  readonly method: string;
-  readonly params?: readonly unknown[] | object;
+  readonly method: string
+  readonly params?: readonly unknown[] | object
 }
 
 interface EIP1193Provider {
-  isStatus?: boolean;
-  host?: string;
-  path?: string;
+  isStatus?: boolean
+  host?: string
+  path?: string
 
-  request: (request: RequestArguments) => Promise<unknown>;
+  request: (request: RequestArguments) => Promise<unknown>
 }
 
 export type WalletProviderDefs = {
-  walletProviderId: string;
-  walletProviderUrl: string;
-};
+  walletProviderId: string
+  walletProviderUrl: string
+}
 
 export type Config = {
-  walletProvider: WalletProviderDefs;
-};
+  walletProvider: WalletProviderDefs
+}
 
 const createWalletProviderService = async (config: Config) =>
   new WalletProviderServiceClient({
     walletProviderId: config.walletProvider.walletProviderId,
-    walletProviderUrl: config.walletProvider.walletProviderUrl,
-  });
+    walletProviderUrl: config.walletProvider.walletProviderUrl
+  })
 
-let selectedBrowserWallet: EIP1193Provider | null = null;
+let selectedBrowserWallet: EIP1193Provider | null = null
 
 // Sign data using the secret key stored on Browser Wallet
 // It creates a popup window, presenting the human readable form of `request`
@@ -81,62 +81,62 @@ export class BrowserWallet implements IBrowserWallet {
     from: string,
     request: TypedData<T>
   ): Promise<unknown> {
-    console.log(selectedBrowserWallet);
+    console.log(selectedBrowserWallet)
 
     //should be provider
     return await selectedBrowserWallet!.request({
       method: "eth_signTypedData_v4",
-      params: [from, JSON.stringify(request)],
-    });
+      params: [from, JSON.stringify(request)]
+    })
   }
 }
 
 // Function to convert hex string to Uint8Array
 function hexToUint8Array(hex: string) {
   if (hex.length % 2 !== 0) {
-    throw new Error("Hex string must have an even number of characters");
+    throw new Error("Hex string must have an even number of characters")
   }
-  const array = new Uint8Array(hex.length / 2);
+  const array = new Uint8Array(hex.length / 2)
   for (let i = 0; i < hex.length; i += 2) {
-    array[i / 2] = Number.parseInt(hex.substr(i, 2), 16);
+    array[i / 2] = Number.parseInt(hex.substr(i, 2), 16)
   }
-  return array;
+  return array
 }
 
 const CreateDanSession: React.FC = () => {
-  const classes = useStyles();
-  const token = configInfo.usdc.address as Hex;
-  const amount = parseUnits("50".toString(), 6);
+  const classes = useStyles()
+  const token = configInfo.usdc.address as Hex
+  const amount = parseUnits("50".toString(), 6)
 
-  const { address: eoa, connector } = useAccount();
-  const client = useClient();
-  const { smartAccountAddress, smartAccountClient } = useSmartAccount();
-  const [session, setSession] = useState<Session | null>(null);
-  const [provider, setProvider] = useState<EIP1193Provider | null>(null);
+  const { address: eoa, connector } = useAccount()
+  const client = useClient()
+  const { smartAccountAddress, smartAccountClient } = useSmartAccount()
+  const [session, setSession] = useState<Session | null>(null)
+  const [provider, setProvider] = useState<EIP1193Provider | null>(null)
 
   useEffect(() => {
     const fetchProvider = async () => {
       if (connector) {
-        const prov = await connector.getProvider();
-        prov && setProvider(prov as EIP1193Provider);
-        selectedBrowserWallet = prov as EIP1193Provider;
+        const prov = await connector.getProvider()
+        prov && setProvider(prov as EIP1193Provider)
+        selectedBrowserWallet = prov as EIP1193Provider
       }
-    };
-    fetchProvider();
-  }, [connector]);
+    }
+    fetchProvider()
+  }, [connector])
 
   const policy: any[] = [
     { interval: { validAfter: 0, validUntil: 0 } },
     "{SESSION_KEY}",
     token,
     eoa!,
-    parseUnits("50".toString(), 6),
-  ];
+    parseUnits("50".toString(), 6)
+  ]
 
   const createDanSessionHandler = async () => {
     try {
       if (!smartAccountClient || !smartAccountAddress) {
-        throw new Error("Smart Account not found");
+        throw new Error("Smart Account not found")
       }
       console.log(
         "use",
@@ -144,22 +144,22 @@ const CreateDanSession: React.FC = () => {
         "address: ",
         smartAccountAddress,
         "to create the session"
-      );
+      )
 
       // const sk = ed.utils.randomPrivateKey();
-      const sk = hexToUint8Array(import.meta.env.VITE_EPHEMERAL_KEY!);
+      const sk = hexToUint8Array(import.meta.env.VITE_EPHEMERAL_KEY!)
       // Global variable for now
-      ephSK = sk;
-      ephPK = await ed.getPublicKeyAsync(sk);
+      ephSK = sk
+      ephPK = await ed.getPublicKeyAsync(sk)
 
       const clusterConfig = {
         walletProvider: {
           walletProviderId: "WalletProvider",
-          walletProviderUrl: "ws://localhost:8090/v1",
-        },
-      };
+          walletProviderUrl: "ws://localhost:8090/v1"
+        }
+      }
 
-      const wpClient = await createWalletProviderService(clusterConfig);
+      const wpClient = await createWalletProviderService(clusterConfig)
 
       // Authenticate using EOA
       const eoaAuth = new EOAAuth(
@@ -168,54 +168,49 @@ const CreateDanSession: React.FC = () => {
         ephPK,
         // Lifetime of one hour
         60 * 60
-      );
+      )
 
-      console.log("ephemeral public key:", ephPK);
+      console.log("ephemeral public key:", ephPK)
 
-      const threshold = 11;
-      const partiesNumber = 20;
+      const threshold = 11
+      const partiesNumber = 20
 
-      const sdk = new NetworkSigner(
-        wpClient,
-        threshold,
-        partiesNumber,
-        eoaAuth
-      );
+      const sdk = new NetworkSigner(wpClient, threshold, partiesNumber, eoaAuth)
 
       // Generate a new key
-      const resp: KeygenResponse = await sdk.authenticateAndCreateKey(ephPK);
+      const resp: KeygenResponse = await sdk.authenticateAndCreateKey(ephPK)
 
-      console.log("keygen response:", resp);
+      console.log("keygen response:", resp)
 
       // resp.publicKey
       // get EOA from public key which will be session key EOA
 
-      const pubKey = resp.publicKey;
+      const pubKey = resp.publicKey
 
       if (pubKey.startsWith("0x")) {
         // const pubKey = resp.publicKey;
       }
 
       // Compute the Keccak-256 hash of the public key
-      const hash = keccak256(("0x" + pubKey) as Hex);
+      const hash = keccak256(("0x" + pubKey) as Hex)
 
       // The Ethereum address is the last 20 bytes of the hash
-      const sessionKeyEOA = ("0x" + hash.slice(-40)) as Hex;
+      const sessionKeyEOA = ("0x" + hash.slice(-40)) as Hex
 
-      console.log("sessionKeyEOA a", sessionKeyEOA);
+      console.log("sessionKeyEOA a", sessionKeyEOA)
 
       const { sessionStorageClient } = await createSessionKeyEOA(
         smartAccountClient,
         getChain(80002)
-      );
+      )
 
       const sessionsModule = await createDANSessionKeyManagerModule({
         smartAccountAddress,
-        sessionStorageClient,
-      });
+        sessionStorageClient
+      })
 
-      console.log("session module");
-      console.log(sessionsModule);
+      console.log("session module")
+      console.log(sessionsModule)
 
       const createSessionDataParams = createERC20SessionDatum({
         interval: { validAfter: 0, validUntil: 0 },
@@ -225,67 +220,67 @@ const CreateDanSession: React.FC = () => {
             { type: "address" },
             { type: "address" },
             { type: "address" },
-            { type: "uint256" },
+            { type: "uint256" }
           ],
           [sessionKeyEOA, token, eoa!, amount]
-        ),
-      });
+        )
+      })
 
       const { data: policyData, sessionIDInfo } =
-        await sessionsModule.createSessionData([createSessionDataParams]);
+        await sessionsModule.createSessionData([createSessionDataParams])
 
-      console.log("policyData", policyData);
-      console.log("sessionIDInfo", sessionIDInfo);
+      console.log("policyData", policyData)
+      console.log("sessionIDInfo", sessionIDInfo)
 
       const permitTx = {
         to: DEFAULT_SESSION_KEY_MANAGER_MODULE,
-        data: policyData,
-      };
+        data: policyData
+      }
 
-      const txs: Transaction[] = [];
+      const txs: Transaction[] = []
 
-      const isDeployed = await smartAccountClient.isAccountDeployed();
+      const isDeployed = await smartAccountClient.isAccountDeployed()
       const enableSessionTx = await smartAccountClient.getEnableModuleData(
         DEFAULT_SESSION_KEY_MANAGER_MODULE
-      );
+      )
 
       if (isDeployed) {
         const enabled = await smartAccountClient.isModuleEnabled(
           DEFAULT_SESSION_KEY_MANAGER_MODULE
-        );
+        )
         if (!enabled) {
-          txs.push(enableSessionTx);
+          txs.push(enableSessionTx)
         }
       } else {
-        txs.push(enableSessionTx);
+        txs.push(enableSessionTx)
       }
 
-      txs.push(permitTx);
+      txs.push(permitTx)
 
       const userOpResponse = await smartAccountClient.sendTransaction(txs, {
-        paymasterServiceData: { mode: PaymasterMode.SPONSORED },
-      });
+        paymasterServiceData: { mode: PaymasterMode.SPONSORED }
+      })
 
-      console.log("userOpResponse", userOpResponse);
+      console.log("userOpResponse", userOpResponse)
 
       const {
         receipt: { transactionHash },
-        success,
-      } = await userOpResponse.wait();
+        success
+      } = await userOpResponse.wait()
 
-      console.log("transactionHash", transactionHash);
+      console.log("transactionHash", transactionHash)
 
       const resultingSession = {
         sessionStorageClient,
-        sessionIDInfo,
-      };
+        sessionIDInfo
+      }
 
       // Handle Success. Keep the "Session" (StorageClient and sessionIDs) and set it to the session
-      success && setSession(resultingSession);
+      success && setSession(resultingSession)
     } catch (error) {
-      console.error("Error creating session:", error);
+      console.error("Error creating session:", error)
     }
-  };
+  }
 
   return (
     <main className={classes.main}>
@@ -319,28 +314,28 @@ const CreateDanSession: React.FC = () => {
         <Button title="Create Session" onClickFunc={createDanSessionHandler} />
       )}
     </main>
-  );
-};
+  )
+}
 
 const useStyles = makeStyles(() => ({
   main: {
     margin: "auto",
     padding: "10px 40px",
-    color: "#EEEEEE",
+    color: "#EEEEEE"
   },
   subTitle: {
     color: "#FFB999",
     fontSize: 36,
-    margin: 0,
+    margin: 0
   },
   h3Title: {
-    color: "#e6e6e6",
+    color: "#e6e6e6"
   },
   listHover: {
     "&:hover": {
-      color: "#FF9551",
-    },
-  },
-}));
+      color: "#FF9551"
+    }
+  }
+}))
 
-export default CreateDanSession;
+export default CreateDanSession
--- src/components/Modules/UseDanSession.tsx
+++ src/components/Modules/UseDanSession.tsx
@@ -1,44 +1,44 @@
 import {
+  ERC20_ABI,
   type Session,
   SessionLocalStorage,
   createDANSessionKeyManagerModule,
-  getChain,
-  ERC20_ABI,
-} from "@biconomy/account";
-import { useSmartAccount } from "@biconomy/use-aa";
-import type React from "react";
-import { useMemo } from "react";
-import "react-toastify/dist/ReactToastify.css";
-import { type Hex, encodeFunctionData, parseAbi, parseUnits } from "viem";
-import { useAccount } from "wagmi";
-import { configInfo } from "../../utils";
-import Button from "../Button";
+  getChain
+} from "@biconomy/account"
+import { useSmartAccount } from "@biconomy/use-aa"
+import type React from "react"
+import { useMemo } from "react"
+import "react-toastify/dist/ReactToastify.css"
+import { type Hex, encodeFunctionData, parseAbi, parseUnits } from "viem"
+import { useAccount } from "wagmi"
+import { configInfo } from "../../utils"
+import Button from "../Button"
 
 interface props {
-  session: Session;
+  session: Session
 }
 
 // Function to convert hex string to Uint8Array
 function hexToUint8Array(hex: string) {
   if (hex.length % 2 !== 0) {
-    throw new Error("Hex string must have an even number of characters");
+    throw new Error("Hex string must have an even number of characters")
   }
-  const array = new Uint8Array(hex.length / 2);
+  const array = new Uint8Array(hex.length / 2)
   for (let i = 0; i < hex.length; i += 2) {
-    array[i / 2] = Number.parseInt(hex.substr(i, 2), 16);
+    array[i / 2] = Number.parseInt(hex.substr(i, 2), 16)
   }
-  return array;
+  return array
 }
 
 const UseDanSession: React.FC<props> = ({ session }) => {
-  const token = configInfo.usdc.address as Hex;
-  const amount = parseUnits("50".toString(), 6);
+  const token = configInfo.usdc.address as Hex
+  const amount = parseUnits("50".toString(), 6)
 
-  const { address: eoa } = useAccount();
-  const { smartAccountAddress, smartAccountClient } = useSmartAccount();
+  const { address: eoa } = useAccount()
+  const { smartAccountAddress, smartAccountClient } = useSmartAccount()
 
-  console.log("use dan session: ", smartAccountAddress);
-  console.log(session);
+  console.log("use dan session: ", smartAccountAddress)
+  console.log(session)
 
   const transactions = useMemo(
     () => ({
@@ -46,46 +46,46 @@ const UseDanSession: React.FC<props> = ({ session }) => {
       data: encodeFunctionData({
         abi: parseAbi(["function transfer(address _to, uint256 _value)"]),
         functionName: "transfer",
-        args: [eoa!, amount],
-      }),
+        args: [eoa!, amount]
+      })
     }),
     [token, eoa, amount]
-  );
+  )
 
   const useDanSessionHandler = async () => {
     if (!smartAccountClient || !smartAccountAddress) {
-      throw new Error("Smart Account not found");
+      throw new Error("Smart Account not found")
     }
     if (!session) {
-      throw new Error("Session not found");
+      throw new Error("Session not found")
     }
 
-    const sessionStorageClient = new SessionLocalStorage(smartAccountAddress);
+    const sessionStorageClient = new SessionLocalStorage(smartAccountAddress)
 
     const sessionsModule = await createDANSessionKeyManagerModule({
       smartAccountAddress,
-      sessionStorageClient,
-    });
+      sessionStorageClient
+    })
 
     // Review if needed. or already baked in
-    smartAccountClient.setActiveValidationModule(sessionsModule as any);
+    smartAccountClient.setActiveValidationModule(sessionsModule as any)
 
     console.log(
       "active validation module ",
       smartAccountClient.activeValidationModule
-    );
+    )
 
-    const sk = hexToUint8Array(import.meta.env.VITE_EPHEMERAL_KEY!);
+    const sk = hexToUint8Array(import.meta.env.VITE_EPHEMERAL_KEY!)
 
-    console.log("use DAN session ever here? =============>");
+    console.log("use DAN session ever here? =============>")
 
-    const sessionID = session.sessionIDInfo[0];
+    const sessionID = session.sessionIDInfo[0]
 
     const sessionKeyEOA = (
       await session.sessionStorageClient.getSessionData({ sessionID })
-    ).sessionPublicKey;
+    ).sessionPublicKey
 
-    console.log("sessionKeyEOA b", sessionKeyEOA);
+    console.log("sessionKeyEOA b", sessionKeyEOA)
 
     // Send the transactions using session params
     const { wait } = await smartAccountClient.sendTransaction(transactions, {
@@ -95,21 +95,21 @@ const UseDanSession: React.FC<props> = ({ session }) => {
         ephSK: sk,
         threshold: 10,
         partiesNumber: 21,
-        sessionKeyEOA,
-      },
-    });
+        sessionKeyEOA
+      }
+    })
     // Wait for the createSessionTx
     const {
       receipt: { transactionHash },
-      success,
-    } = await wait();
+      success
+    } = await wait()
 
-    success && console.log({ transactionHash });
+    success && console.log({ transactionHash })
 
     // Handle Success....
-  };
+  }
 
-  return <Button title="Minft NFT" onClickFunc={useDanSessionHandler} />;
-};
+  return <Button title="Minft NFT" onClickFunc={useDanSessionHandler} />
+}
 
-export default UseDanSession;
+export default UseDanSession
