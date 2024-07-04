edfd6845b48c13b8a1cab3db798ef1c95856761f
Joe Pegler <joepegler123@gmail.com>
2024-07-04 17:37:48 +0100

    chore: lint

--- src/components/Modules/CreateDanSession.tsx
+++ src/components/Modules/CreateDanSession.tsx
@@ -2,25 +2,25 @@ import {
   BrowserWallet,
   type PolicyWithoutSessionKey,
   type Session,
-  createDistributedSession,
-} from "@biconomy/account";
-import { bigIntReplacer, useSmartAccount } from "@biconomy/use-aa";
-import { makeStyles } from "@mui/styles";
-import type React from "react";
-import { useState } from "react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import type { EIP1193Provider, Hex } from "viem";
-import Button from "../Button";
-import UseDanSession from "./UseDanSession";
-import { useAccount } from "wagmi";
+  createDistributedSession
+} from "@biconomy/account"
+import { bigIntReplacer, useSmartAccount } from "@biconomy/use-aa"
+import { makeStyles } from "@mui/styles"
+import type React from "react"
+import { useState } from "react"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import type { EIP1193Provider, Hex } from "viem"
+import { useAccount } from "wagmi"
+import Button from "../Button"
+import UseDanSession from "./UseDanSession"
 
 const CreateDanSession: React.FC = () => {
-  const classes = useStyles();
-  const nftAddress: Hex = "0x1758f42Af7026fBbB559Dc60EcE0De3ef81f665e";
-  const { smartAccountAddress, smartAccountClient } = useSmartAccount();
-  const [session, setSession] = useState<Session | null>(null);
-  const { connector } = useAccount();
+  const classes = useStyles()
+  const nftAddress: Hex = "0x1758f42Af7026fBbB559Dc60EcE0De3ef81f665e"
+  const { smartAccountAddress, smartAccountClient } = useSmartAccount()
+  const [session, setSession] = useState<Session | null>(null)
+  const { connector } = useAccount()
 
   const policy: PolicyWithoutSessionKey[] = [
     {
@@ -30,35 +30,35 @@ const CreateDanSession: React.FC = () => {
         {
           offset: 0,
           condition: 0,
-          referenceValue: smartAccountAddress,
-        },
+          referenceValue: smartAccountAddress
+        }
       ],
       interval: {
         validUntil: 0,
-        validAfter: 0,
+        validAfter: 0
       },
-      valueLimit: 0n,
-    },
-  ];
+      valueLimit: 0n
+    }
+  ]
 
   const createDanSessionHandler = async () => {
     try {
       if (!smartAccountClient || !smartAccountAddress || !connector) {
-        throw new Error("Smart Account not found");
+        throw new Error("Smart Account not found")
       }
 
       const { wait, session } = await createDistributedSession(
         smartAccountClient,
         policy
-      );
+      )
 
-      const { success } = await wait();
+      const { success } = await wait()
 
-      success && setSession(session);
+      success && setSession(session)
     } catch (error) {
-      console.error("Error creating session:", error);
+      console.error("Error creating session:", error)
     }
-  };
+  }
 
   return (
     <main className={classes.main}>
@@ -92,28 +92,28 @@ const CreateDanSession: React.FC = () => {
         <Button title="Create Session" onClickFunc={createDanSessionHandler} />
       )}
     </main>
-  );
-};
+  )
+}
 
 const useStyles = makeStyles(() => ({
   main: {
     margin: "auto",
     padding: "10px 40px",
-    color: "#EEEEEE",
+    color: "#EEEEEE"
   },
   subTitle: {
     color: "#FFB999",
     fontSize: 36,
-    margin: 0,
+    margin: 0
   },
   h3Title: {
-    color: "#e6e6e6",
+    color: "#e6e6e6"
   },
   listHover: {
     "&:hover": {
-      color: "#FF9551",
-    },
-  },
-}));
+      color: "#FF9551"
+    }
+  }
+}))
 
-export default CreateDanSession;
+export default CreateDanSession
