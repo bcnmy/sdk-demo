3269d33bd9968af8cf8b38df300b2ce4561e8c8c
Joe Pegler <joepegler123@gmail.com>
2024-07-04 17:22:26 +0100

    chore: lint

--- src/components/Modules/UseDanSession.tsx
+++ src/components/Modules/UseDanSession.tsx
@@ -3,43 +3,43 @@ import {
   type Session,
   type Transaction,
   createSessionSmartAccountClient,
-  getChain,
-} from "@biconomy/account";
-import { useSmartAccount } from "@biconomy/use-aa";
-import type React from "react";
-import "react-toastify/dist/ReactToastify.css";
-import { type Hex, encodeFunctionData, parseAbi } from "viem";
-import { biconomyPaymasterApiKey, bundlerUrl } from "../../utils/chainConfig";
-import Button from "../Button";
+  getChain
+} from "@biconomy/account"
+import { useSmartAccount } from "@biconomy/use-aa"
+import type React from "react"
+import "react-toastify/dist/ReactToastify.css"
+import { type Hex, encodeFunctionData, parseAbi } from "viem"
+import { biconomyPaymasterApiKey, bundlerUrl } from "../../utils/chainConfig"
+import Button from "../Button"
 
 const withSponsorship = {
-  paymasterServiceData: { mode: PaymasterMode.SPONSORED },
-};
+  paymasterServiceData: { mode: PaymasterMode.SPONSORED }
+}
 interface props {
-  session: Session;
+  session: Session
 }
 
 const UseDanSession: React.FC<props> = ({ session }) => {
-  const nftAddress: Hex = "0x1758f42Af7026fBbB559Dc60EcE0De3ef81f665e";
-  const chain = getChain(80002);
+  const nftAddress: Hex = "0x1758f42Af7026fBbB559Dc60EcE0De3ef81f665e"
+  const chain = getChain(80002)
 
-  const { smartAccountAddress } = useSmartAccount();
+  const { smartAccountAddress } = useSmartAccount()
 
   const nftMintTx: Transaction = {
     to: nftAddress,
     data: encodeFunctionData({
       abi: parseAbi(["function safeMint(address _to)"]),
       functionName: "safeMint",
-      args: [smartAccountAddress],
-    }),
-  };
+      args: [smartAccountAddress]
+    })
+  }
 
   const useDanSessionHandler = async () => {
     if (!smartAccountAddress) {
-      throw new Error("Smart Account not found");
+      throw new Error("Smart Account not found")
     }
     if (!session) {
-      throw new Error("Session not found");
+      throw new Error("Session not found")
     }
 
     const smartAccountWithSession = await createSessionSmartAccountClient(
@@ -47,28 +47,28 @@ const UseDanSession: React.FC<props> = ({ session }) => {
         accountAddress: smartAccountAddress, // Set the account address on behalf of the user
         biconomyPaymasterApiKey,
         bundlerUrl,
-        chainId: chain.id,
+        chainId: chain.id
       },
       session,
       "DAN"
-    );
+    )
 
     // Send the transactions using session params
     const { wait } = await smartAccountWithSession.sendSessionTransaction(
       [session, chain, null],
       nftMintTx,
       withSponsorship
-    );
+    )
 
     const {
       receipt: { transactionHash },
-      success,
-    } = await wait();
+      success
+    } = await wait()
 
-    success && console.log({ transactionHash });
-  };
+    success && console.log({ transactionHash })
+  }
 
-  return <Button title="Minft NFT" onClickFunc={useDanSessionHandler} />;
-};
+  return <Button title="Minft NFT" onClickFunc={useDanSessionHandler} />
+}
 
-export default UseDanSession;
+export default UseDanSession
